3fe49692025d173bfa4503f65e90f3dc82132317
mota.fernandomaia@gmail.com <mota.fernandomaia@gmail.com>
2015-05-07 11:34:15 -0300

    version multi-columns

--- rowGroup.js
+++ rowGroup.js
@@ -6,25 +6,45 @@
     
     defaults: {
         decimal: true,
-		textFormat: function(v, st, opt, td) {		
+    	textFormat: function(v, st, opt, td) {		
 		    if($(td).attr("display")!='none'){
                 
                 var row = st.rowIdx + 1;
                 var countRows = 1;
-               
-                
-                while(st.tableData[row][st.colIdx] == v && row < st.rawData.resultset.length-1){                 
-                    var nextTd = $('#tabela_funcaoTable tbody tr').eq(row).find('td:eq('+st.colIdx+')')[0];
-                    if(nextTd){
-                        $(nextTd).css('display','none'); //remove();     
-                        $(nextTd).attr('display','none');
+                var equalLine=true;
+                while(st.tableData[row][st.colIdx] == v && row < st.rawData.resultset.length-1 && equalLine==true){
+                    
+                    if(st.colIdx>0){
+                        
+                        var pCol = st.colIdx-1;
+                        while(equalLine == true && pCol >=0){
+                            if(st.tableData[row-1][pCol] != st.tableData[row][pCol])
+                                equalLine = false;   
+                            pCol--;
+                        }
+
+                        if(equalLine == true){
+                            var nextTd = $('#tabela_funcaoTable tbody tr').eq(row).find('td:eq('+st.colIdx+')')[0];
+                            if(nextTd){
+                                $(nextTd).css('display','none'); //remove();     
+                                $(nextTd).attr('display','none');
+                            }
+                            row++;
+                            countRows++;
+                        }
+                    }else{
+                        var nextTd = $('#tabela_funcaoTable tbody tr').eq(row).find('td:eq('+st.colIdx+')')[0];
+                        if(nextTd){
+                            $(nextTd).css('display','none'); //remove();     
+                            $(nextTd).attr('display','none');
+                        }
+                        row++;
+                        countRows++;
                     }
-                    row++;
-                    countRows++;
                 }
                 
                 //verify last row
-                if(st.tableData[row][st.colIdx] == v){
+                if(st.tableData[row][st.colIdx] == v && equalLine==true){
                     var nextTd = $('#tabela_funcaoTable tbody tr').eq(row).find('td:eq('+st.colIdx+')')[0];
                     if(nextTd){
                         $(nextTd).css('display','none'); //remove();     
